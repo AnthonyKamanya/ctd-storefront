df9a892155617ddd48b70855193bcf72b8cb085f
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-19 00:24:08 +0100

    wip. remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import inventoryData from './assets/catalog.json';
 import './App.css';
 import Header from './Header';
 import ProductList from './ProductList';
-import ProductCard from './ProductCard';
 
 function App() {
   const [inventory, setInventory] = useState([]);
@@ -12,17 +11,15 @@ function App() {
     setInventory([...inventoryData.inventory]);
   }, []);
   const [cart, setCart] = useState([]);
+
   function handleAddItemToCart(id) {
     const target = inventory.find((item) => item.id === id);
-    //if no inventory items are found
-    //we want to prevent the app from crashing
-    //by exiting this function now
+
     if (!target) {
       console.error('cart error: item not found');
       return;
     }
-    //create an new object, spread the contents of the item selected
-    //and add a `cartItemId`
+
     const cartItem = { ...target, cartItemId: Date.now() };
     console.log(cartItem);
     setCart([...cart, cartItem]);
@@ -36,20 +33,13 @@ function App() {
     setCart([...updatedCart]);
   }
 
-  function promoteItem() {
-    return (
-      <ProductCard
-        name="Special Limited Edition Tee!"
-        description="Special limited edition neon green shirt with a metallic 
-        Code the Dream Logo shinier than the latest front-end framework! 
-        Signed by the legendary Frank!"
-      />
-    );
-  }
   return (
     <main className="App">
-      <Header />
-      <ProductList inventory={inventory}>{promoteItem()}</ProductList>
+      <Header cart={cart}/>
+      <ProductList
+        inventory={inventory}
+        handleAddItemToCart={handleAddItemToCart}
+      ></ProductList>
     </main>
   );
 }
