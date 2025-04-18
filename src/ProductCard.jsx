6abc856029be97a19ab4f4c83fca1f2e7f19222e
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-19 00:24:35 +0100

    wip passing prop to child component

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,9 +1,15 @@
-function ProductCard({ name, description }) {
+function ProductCard({ name, description, id, handleAddItemToCart }) {
   return (
     <li>
       <div className="itemCard">
         <h2>{name}</h2>
         <p>{description}</p>
+        <button
+          className="productButton"
+          onClick={() => handleAddItemToCart(id)}
+        >
+          Add to Cart
+        </button>
       </div>
     </li>
   );
