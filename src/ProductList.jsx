da3346737bb7b7118804755be48e7fb8c3079392
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-19 00:24:56 +0100

    wip: passing props

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,13 +1,15 @@
 import ProductCard from './ProductCard';
 
-function ProductList({ inventory, children }) {
+function ProductList({ inventory,handleAddItemToCart}) {
   return (
-    <ul>
-      {children} 
+    <ul className="ProductList">
+      
       {inventory.map((item) => {
         return (
           <ProductCard
+            handleAddItemToCart={handleAddItemToCart}
             key={item.id}
+            id={item.id}
             name={item.baseName}
             description={item.baseDescription}
           />
