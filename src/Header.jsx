a57af1a947cc4ad03fc1b328debc2cdb3cdeabaa
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-19 00:47:24 +0100

    wip: update the Header so that it prints the shopping cart

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,27 @@
-import logo from './assets/react.svg'
+import logo from './assets/react.svg';
+import shoppingCart from './assets/shoppingCart.svg';
+import { useEffect } from 'react';
 
-function Header() {
+function Header({cart}) {
+  useEffect(() => {
+    cart.forEach((item) => {
+      console.log(item.baseName, item.cartItemId);
+    });
+    if (cart.length > 0) {
+      console.log('--end of cart--');
+    }
+  });
   return (
     <div className="coming-soon">
       <h1>CTD Swag</h1>
       <div style={{ height: 100, width: 100 }}>
         <img src={logo} alt="Code The Dream Logo" />
       </div>
+      <div className="shoppingCart">
+        <img src={shoppingCart} alt="" />
+      </div>
     </div>
   );
 }
 
-export default Header
+export default Header;
