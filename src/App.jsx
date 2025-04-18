a5004eb43319d591ba9a4afc3c1eda4152387178
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-18 22:16:34 +0100

    wip: add cart functionalities add item to the cart add event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import inventoryData from './assets/catalog.json';
 import './App.css';
 import Header from './Header';
@@ -6,12 +6,40 @@ import ProductList from './ProductList';
 import ProductCard from './ProductCard';
 
 function App() {
-  const [inventory, setInventory] = useState(inventoryData.inventory);
+  const [inventory, setInventory] = useState([]);
+
+  useEffect(() => {
+    setInventory([...inventoryData.inventory]);
+  }, []);
+  const [cart, setCart] = useState([]);
+  function handleAddItemToCart(id) {
+    const target = inventory.find((item) => item.id === id);
+    //if no inventory items are found
+    //we want to prevent the app from crashing
+    //by exiting this function now
+    if (!target) {
+      console.error('cart error: item not found');
+      return;
+    }
+    //create an new object, spread the contents of the item selected
+    //and add a `cartItemId`
+    const cartItem = { ...target, cartItemId: Date.now() };
+    console.log(cartItem);
+    setCart([...cart, cartItem]);
+  }
+  function addItemToCart(item) {
+    setCart([...cart, item]);
+  }
+
+  function removeItemFromCart(id) {
+    const updatedCart = cart.filter((item) => !item.id === id);
+    setCart([...updatedCart]);
+  }
 
   function promoteItem() {
     return (
       <ProductCard
-        name="Limited Edition Tee!"
+        name="Special Limited Edition Tee!"
         description="Special limited edition neon green shirt with a metallic 
         Code the Dream Logo shinier than the latest front-end framework! 
         Signed by the legendary Frank!"
@@ -19,9 +47,9 @@ function App() {
     );
   }
   return (
-    <main>
+    <main className="App">
       <Header />
-      <ProductList inventory={inventory} >{promoteItem()}</ProductList>
+      <ProductList inventory={inventory}>{promoteItem()}</ProductList>
     </main>
   );
 }
