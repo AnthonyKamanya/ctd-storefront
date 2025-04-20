7fece6f7847c66953a42917e455b965ae89d43f8
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-20 18:09:55 +0100

    feat: conditional render to display more option if the product variation is more than 1

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,15 +1,18 @@
-function ProductCard({ name, description, id, handleAddItemToCart }) {
+function ProductCard({ name, description, product, handleAddItemToCart }) {
   return (
     <li>
       <div className="itemCard">
         <h2>{name}</h2>
         <p>{description}</p>
-        <button
-          className="productButton"
-          onClick={() => handleAddItemToCart(id)}
-        >
-          Add to Cart
-        </button>
+        <div className="productButtons">
+          {product.variants.length > 1 ? (
+            <button>Show Option</button>
+          ) : (
+            <button onClick={() => handleAddItemToCart(product.variants[0].id)}>
+              Add to Cart
+            </button>
+          )}
+        </div>
       </div>
     </li>
   );
