755b2a6e7dcde9c28bdf8efc5f4c987068870948
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-04-21 23:01:51 +0100

    wip: introduce variant option feature on the product card

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
+import ProductCardVariants from './ProductCardVariants';
+import placeholder from "./assets/placeholder.png"
+
 function ProductCard({ name, description, product, handleAddItemToCart }) {
+  const [areVariantsShown, setAreVariantsShown] = useState();
   return (
-    <li>
+    <li className="productCard">
+      <div className="productPreview">
+      <img src={placeholder} alt=" " />
+    </div>
       <div className="itemCard">
         <h2>{name}</h2>
         <p>{description}</p>
-        <div className="productButtons">
-          {product.variants.length > 1 ? (
-            <button>Show Option</button>
-          ) : (
-            <button onClick={() => handleAddItemToCart(product.variants[0].id)}>
-              Add to Cart
-            </button>
-          )}
-        </div>
       </div>
+      <div className="productButtons">
+        {product.variants.length > 1 ? (
+          <button onClick={() => setAreVariantsShown(true)}>Show Option</button>
+        ) : (
+          <button onClick={() => handleAddItemToCart(product.variants[0].id)}>
+            Add to Cart
+          </button>
+        )}
+      </div>
+      {areVariantsShown && (
+        <ProductCardVariants
+          handleAddItemToCart={handleAddItemToCart}
+          variants={product.variants}
+          closeVariants={() => setAreVariantsShown(false)}
+        />
+      )}
     </li>
   );
 }
